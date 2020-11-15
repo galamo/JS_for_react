67c798918fb441e44b5ee33657d18d03e9e8fdcc
Gal Amouyal <gal.amouyal@ibm.com>
2020-11-15 21:18:26 +0200

    add ugly card

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ const cars = [
     Acceleration: 10,
     Year: "1970-01-01",
     Origin: "USA",
+    image:
+      "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
   },
   {
     Name: "chevrolet impala",
@@ -177,184 +179,6 @@ const cars = [
   },
 ];
 
-const cars2 = [
-  {
-    Name: "chevrolet chevelle malibu",
-    Miles_per_Gallon: 18,
-    Cylinders: 8,
-    Displacement: 307,
-    Horsepower: 130,
-    Weight_in_lbs: 3504,
-    Acceleration: 12,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "buick skylark 320",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 350,
-    Horsepower: 165,
-    Weight_in_lbs: 3693,
-    Acceleration: 11.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "plymouth satellite",
-    Miles_per_Gallon: 18,
-    Cylinders: 8,
-    Displacement: 318,
-    Horsepower: 150,
-    Weight_in_lbs: 3436,
-    Acceleration: 11,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "amc rebel sst",
-    Miles_per_Gallon: 16,
-    Cylinders: 8,
-    Displacement: 304,
-    Horsepower: 150,
-    Weight_in_lbs: 3433,
-    Acceleration: 12,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "ford torino",
-    Miles_per_Gallon: 17,
-    Cylinders: 8,
-    Displacement: 302,
-    Horsepower: 140,
-    Weight_in_lbs: 3449,
-    Acceleration: 10.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "ford galaxie 500",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 429,
-    Horsepower: 198,
-    Weight_in_lbs: 4341,
-    Acceleration: 10,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "chevrolet impala",
-    Miles_per_Gallon: 14,
-    Cylinders: 8,
-    Displacement: 454,
-    Horsepower: 220,
-    Weight_in_lbs: 4354,
-    Acceleration: 9,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "plymouth fury iii",
-    Miles_per_Gallon: 14,
-    Cylinders: 8,
-    Displacement: 440,
-    Horsepower: 215,
-    Weight_in_lbs: 4312,
-    Acceleration: 8.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "pontiac catalina",
-    Miles_per_Gallon: 14,
-    Cylinders: 8,
-    Displacement: 455,
-    Horsepower: 225,
-    Weight_in_lbs: 4425,
-    Acceleration: 10,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "amc ambassador dpl",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 390,
-    Horsepower: 190,
-    Weight_in_lbs: 3850,
-    Acceleration: 8.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "citroen ds-21 pallas",
-    Miles_per_Gallon: null,
-    Cylinders: 4,
-    Displacement: 133,
-    Horsepower: 115,
-    Weight_in_lbs: 3090,
-    Acceleration: 17.5,
-    Year: "1970-01-01",
-    Origin: "Europe",
-  },
-  {
-    Name: "chevrolet chevelle concours (sw)",
-    Miles_per_Gallon: null,
-    Cylinders: 8,
-    Displacement: 350,
-    Horsepower: 165,
-    Weight_in_lbs: 4142,
-    Acceleration: 11.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "ford torino (sw)",
-    Miles_per_Gallon: null,
-    Cylinders: 8,
-    Displacement: 351,
-    Horsepower: 153,
-    Weight_in_lbs: 4034,
-    Acceleration: 11,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "plymouth satellite (sw)",
-    Miles_per_Gallon: null,
-    Cylinders: 8,
-    Displacement: 383,
-    Horsepower: 175,
-    Weight_in_lbs: 4166,
-    Acceleration: 10.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "amc rebel sst (sw)",
-    Miles_per_Gallon: null,
-    Cylinders: 8,
-    Displacement: 360,
-    Horsepower: 175,
-    Weight_in_lbs: 3850,
-    Acceleration: 11,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "dodge challenger se",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 383,
-    Horsepower: 170,
-    Weight_in_lbs: 3563,
-    Acceleration: 10,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-];
 // for loops ?
 // for
 // while
@@ -366,9 +190,27 @@ const cars2 = [
 
 // 1. fetch all the cars models + origins
 
+const carsWithImages = cars.map((car, index) => {
+  if (index === 5) {
+    return {
+      ...car,
+      engine: 4000,
+      image:
+        "https://resize.indiatvnews.com/en/resize/newbucket/715_-/2019/12/mustang-1-1577523921.jpg",
+    };
+  } else {
+    return {
+      ...car,
+      engine: 4000,
+      image:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bugatti-chiron-pur-sport-106-1582836604.jpg",
+    };
+  }
+});
+
 function getPartialCars(cars) {
   return cars.map((car) => {
-    return { model: car.Name, origin: car.Origin };
+    return { model: car.Name, origin: car.Origin, image: car.image };
   });
 }
 
@@ -378,14 +220,35 @@ function getCarsHeader(car) {
   return carHeader;
 }
 
+function getCarImage(imageUrl) {
+  if (!imageUrl) return "No image";
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  img.height = 200;
+  img.width = 300;
+  return img;
+}
+
+function getCarCard(car) {
+  const card = document.createElement("div");
+  const h3 = getCarsHeader(car);
+  const carImage = getCarImage(car.image);
+  //   card.calssName = "carBackground carColor"; same as row 237
+  card.classList.add("card");
+  card.append(h3, carImage);
+
+  return card;
+}
+
 function getCarsHeaders(cars) {
   const headers = cars.map((car) => {
-    return getCarsHeader(car);
+    return getCarCard(car);
   });
   return headers;
 }
 
-const result = getPartialCars(cars);
+const result = getPartialCars(carsWithImages);
 const h3Array = getCarsHeaders(result);
 const container = document.getElementById("container");
+console.log(h3Array);
 container.append(...h3Array);
